28930d2d24f138a6b19642562b4be5f701662aa5
henningfutrell <henning.futrell@protonmail.com>
2019-02-14 10:30:46 -0600

    Rename to basic_bulid dir

---
+++ basic_build/Database.cpp
@@ -0,0 +1,30 @@
+
+#include "Database.h"
+
+void Database::add(string tableName, Table table) {
+}
+
+void Database::drop(string tableName) {
+
+}
+
+void Database::save(string fileName) {
+
+}
+
+Database Database::load(string tableName) {
+    return Database();
+}
+
+vector<string> Database::getTableNames() {
+    return vector<string>();
+}
+
+vector<Table> Database::getTables() {
+    return vector<Table>();
+}
+
+void Database::query(string query) {
+
+}
+
---
+++ basic_build/main.cpp
@@ -0,0 +1,72 @@
+
+#include "Database.h"
+#include "Table.h"
+#include "Record.h"
+
+int main() {
+
+ //////////////////////////////////////////////////////////////////////////////////
+ //
+ // Record
+ //
+ //////////////////////////////////////////////////////////////////////////////////
+
+ Record r(3);
+ cout << "RECORD SIZE " << r.size() << endl;
+ r[2] = "John";
+
+ string s = r[2];
+ cout << s << endl;
+
+ vector<string> attributes = {"id", "name", "birthdate", "university"};
+ string attribute_name = "attribute name";
+ string record = "record";
+ string table1 = "table1";
+ string table2 = "table2";
+
+
+ //////////////////////////////////////////////////////////////////////////////////
+ //
+ // Table
+ //
+ //////////////////////////////////////////////////////////////////////////////////
+
+ Table t(attributes);
+
+ string attribute = attributes[0];
+ t.count(attribute);
+ t.deleteAttribute(attribute);
+ t.insert(r);
+ t.addAttribute(attribute_name);
+ t.getAttributes();
+ t.getSize();
+ t.getRecords();
+ t.makeKey(attribute_name);
+ t.count(attribute);
+ t.max(attribute);
+ t.min(attribute);
+
+ Table t1;
+ Table t2;
+ t.naturalJoin(t1, t2);
+ t.crossJoin(t1, t2);
+
+
+ //////////////////////////////////////////////////////////////////////////////////
+ //
+ // Database
+ //
+ //////////////////////////////////////////////////////////////////////////////////
+
+ Database d;
+
+ string tableName = "test_table";
+
+ d.add(tableName, t);
+ d.save(tableName);
+ d.load(tableName);
+ d.getTableNames();
+ d.getTables();
+ d.query("SELECT * WHERE this GT bananas");
+ d.drop(tableName);
+}
